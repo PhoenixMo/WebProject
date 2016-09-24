1cbf0c6dc3a38b8e468438f23b2638ef47d3e427
PhoenixMo <1002375424@qq.com>
2016-09-24 16:06:02 +0800

    我累个交累个图片！

--- out/main.js
+++ out/main.js
@@ -5,6 +5,26 @@ var Greeter = (function () {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        var Star = "*";
+        var Space = " ";
+        console.log("就先...强行只输5行吧");
+        var Line = 5;
+        hua(Line);
+        function hua(Line) {
+            for (var i = 1; i <= Line; i++) {
+                var Drawline = "";
+                for (var j = 1; j <= (Line + i - 1); j++) {
+                    if ((Line - i + 1) <= j && j <= (Line + i - 1)) {
+                        Drawline = Drawline + Star;
+                    }
+                    else {
+                        Drawline = Drawline + Space;
+                    }
+                }
+                console.log(Drawline);
+            }
+        }
+        console.log("END");
     }
     Greeter.prototype.start = function () {
         var _this = this;
--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,32 @@ class Greeter {
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+var Star="*";
+var Space=" ";
+
+ console.log("就先...强行只输5行吧");
+var Line:number =5;
+hua(Line);
+function hua(Line:number):void {
+    for(var i=1;i<=Line;i++) {
+        var Drawline="";
+        for(var j=1;j<=(Line+i-1);j++) {
+          if((Line-i+1)<=j&&j<=(Line+i-1)) {
+                Drawline=Drawline+Star;
+           }else {
+              Drawline=Drawline+Space;
+           }
+       }
+      console.log(Drawline);
+    }
+}
+console.log("END");
+
     }
 
     start() {
         this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+        
     }
 
     stop() {
@@ -25,4 +47,7 @@ window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
+
+
+    
 };
